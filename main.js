87b61202f98fb4b60da880f8a18edcfff7e6640d
fl-sll <edwardalvin@gmail.com>
2023-02-28 10:57:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+
+for (const link of navLinks) {
+  link.addEventListener('click', smoothScroll);
+}
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute('href');
+  const targetPosition = document.querySelector(targetId).offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1000;
+  let start = null;
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutQuad(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+}
